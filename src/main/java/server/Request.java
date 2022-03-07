fc7f398f1f4d42821b46057b05cf20bbcd13387c
yunCrush <yunfei1577266641@gmail.com>
2022-03-07 13:42:55 +0800

    v2 commit

---
+++ src/main/java/server/Request.java
@@ -0,0 +1,9 @@
+package server;
+
+/**
+ * Author: yunCrush
+ * Date: 2022/3/7 11:16
+ * Description:
+ */
+public class Request {
+}
---
+++ src/main/java/server/StaticResourceUtil.java
@@ -0,0 +1,9 @@
+package server;
+
+/**
+ * Author: yunCrush
+ * Date: 2022/3/7 13:25
+ * Description:
+ */
+public class StaticResourceUtil {
+}
